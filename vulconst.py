8798e2c75ab9f1d04adf8a3faf4547c3ef45eb4d
David McFadzean <davidmc@gmail.com>
2019-06-16 09:21:28 -0400

    Renamed identifiers

--- vulconst.py
+++ vulconst.py
@@ -2,29 +2,30 @@
 
 from bigfloat import *
 
-VN = ""
+VulcanBinary = ""
 
-def om(S, W, R):
-    global VN
+def GenerateBinary(S, W, R):
+    global VulcanBinary
 
     if R == 0:
-        VN = VN + S
+        VulcanBinary = VulcanBinary + S
     else:
-        om(S + "0", W, R-1)
-        om(S + "1", W, R-1)
+        GenerateBinary(S + "0", W, R-1)
+        GenerateBinary(S + "1", W, R-1)
 
 for i in range(1,9):
-    om("", i, i)
+    GenerateBinary("", i, i)
 
-print '.' + VN
+print '.' + VulcanBinary
+print len(VulcanBinary)
 
 with precision(2000):
-    V = BigFloat(0)
+    VulcanDecimal = BigFloat(0)
     b = BigFloat(1)
 
-    for c in VN:
+    for c in VulcanBinary:
         b = b/2.
         if c == '1':
-            V = V+b
+            VulcanDecimal = VulcanDecimal+b
 
-print V
+print VulcanDecimal
